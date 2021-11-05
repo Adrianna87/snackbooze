19cdfdfa3992241820e2d355773ec13c3c61f1fe
Adrianna <adriannam@workmail.com>
2021-11-05 11:21:57 -0400

    unroll

--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -2,8 +2,12 @@ const unroll = require("./unroll");
 
 describe("#unroll", function () {
 
-  it("is a function", function () {
-    expect(typeof unroll).toEqual("function");
+  it("should create new array", function () {
+    expect(unroll([
+      ["A", "B", "C"],
+      ["D", "E", "F"],
+      ["G", "H", "I"]
+    ])).toEqual(["A", "B", "C", "F", "I", "H", "G", "D", "E"]);
   });
 
 });
